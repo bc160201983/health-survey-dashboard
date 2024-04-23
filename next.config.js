1dd218b27138c1a866afff0ddc4340c305cad888
Muhammad Bilal <softnacuk@gmail.com>
2024-04-23 18:43:29 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_KEY: "AIzaSyBCbarwY5FQZ0hY0iufyULVMjKMzZoRNj0",
+    AUTH_DOMAIN: "health-app-react.firebaseapp.com",
+    PROJECT_ID: "health-app-react",
+    STORAGE_BUCKET: "health-app-react.appspot.com",
+    MESSAGE_SENDER_ID: "912573667215",
+    APP_ID: "1:912573667215:web:3012fd42a8ec64c7e31dfa",
+    MEASUREMENT_ID: "G-7LM1B2RCS7",
+  },
   images: {
     remotePatterns: [
       {
